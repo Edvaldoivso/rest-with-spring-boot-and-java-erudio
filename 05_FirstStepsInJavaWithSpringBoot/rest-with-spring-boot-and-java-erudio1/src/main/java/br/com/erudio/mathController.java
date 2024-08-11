2fdb624741d5c3dd2eb58edbae7565c6b5133e96
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2024-08-11 07:16:36 -0300

    Add: Finalizado as novas Operações do exercicio de Spring

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio1/src/main/java/br/com/erudio/mathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio1/src/main/java/br/com/erudio/mathController.java
@@ -15,35 +15,83 @@ public class mathController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	
-	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
 	public Double sum(@PathVariable(value = "numberOne") String numberOne,
-			@PathVariable(value = "numberTwo") String numberTwo
-			) throws Exception {
-		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
-			throw new UnsupportedMathOperationException("Por favor insira os parametros corretamente");
-		}	
-		
-		return convertToDouble(numberOne)+ convertToDouble(numberTwo);
-		
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor insira os parametros corretamente para soma");
+		}
+
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+
 	};
 	
+	
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sub(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor insira os parametros corretamente subtração");
+		}
+
+		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+
+	};
+	
+	
+	
+	@RequestMapping(value = "/mul/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double mul(@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new UnsupportedMathOperationException("Por favor insira os parametros corretamente para a mutiplicação");
+		}
+
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+
+	};
+	
+	
+	
+	
+	@RequestMapping(value = "/raiz/{numberOne}", method = RequestMethod.GET)
+	public Double raiz(
+			@PathVariable(value = "numberOne") String numberOne)
+			 throws Exception {
+		if (!isNumeric(numberOne)) {
+			throw new UnsupportedMathOperationException("Insira um numero para a raiz quadrada");
+		}
+
+		return (double) Math.round(Math.sqrt(convertToDouble(numberOne)));
+
+	};
+	
+	
+	
+	
+	
+	
+
 	private Double convertToDouble(String strNumber) {
-		if(strNumber == null)return 0D;
-		String number = strNumber.replaceAll(",",".");
-		if(isNumeric(number))return Double.parseDouble(number);
-		return  0D;
+		if (strNumber == null)
+			return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+		return 0D;
 	}
-	
-	
+
 	private boolean isNumeric(String strNumber) {
-		
-		if(strNumber == null)return false;
-		String number = strNumber.replaceAll(",",".");
-		
+
+		if (strNumber == null)
+			return false;
+		String number = strNumber.replaceAll(",", ".");
+
 		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
-	
-	
+
 }
